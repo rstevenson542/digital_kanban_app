a1fc13c33d6a77aaba387033a4868d56385fac73
Robert Stevenson <rstevenson542@gmail.com>
2012-06-30 21:14:01 -0500

    removed security issue

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -10,14 +10,6 @@ class TicketsController < ApplicationController
     @ticket = @project.tickets(params[:id])
   end
   
-  def lighthouse_auth
-    Lighthouse.account = 't1dfirst'
-    Lighthouse.token = 'fad70b09c02cf13c6bf307508cec2465d7eb3840'
-  end
   
-  def project_resources
-    @project = Lighthouse::Project.find('75196-myglu')
-    @tickets = @project.tickets
-  end
 
 end
